313edb7af88a2aaec0d51337485e64d382c3986a
Ishan Goel <igoel.mail@gmail.com>
2021-02-21 08:51:04 +0400

    Add meta tags for correct social media previews

---
+++ updated_sources/multer.js
@@ -0,0 +1,17 @@
+// MULTER AND CLOUNINARY CONFIG
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + file.originalname)
+  }
+})
+const imageFilter = function (req, file, cb) {
+  // accept image files only
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+    return cb(new Error('Only jpg, jpeg, png and gif image files are allowed'), false)
+  }
+  cb(null, true)
+}
+
+module.exports = multer({ storage: storage, fileFilter: imageFilter })
---
+++ updated_sources/routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router({ mergeParams: true })
+
+const middleware = require('../middleware/index')
+
+const Message = require('../models/message.js')
+
+router.get('/', middleware.isNotLoggedIn, (req, res) => {
+  res.render('index')
+})
+
+router.post('/messages', (req, res) => {
+  Message.create(req.body.message, (err, msg) => {
+    if (err) {
+      console.log(err)
+      req.flash('error', err.message)
+      return res.redirect('/')
+    }
+    req.flash('success', 'Your message has been sent.')
+    return res.redirect('/')
+  })
+})
+
+module.exports = router
