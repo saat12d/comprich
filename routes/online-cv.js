ee6c1a6143924ab195561996061d573cd18809d4
Saatvik <saatvik.s12@gmail.com>
2021-11-03 10:47:38 +0400

    make online cv page dynamic

--- routes/online-cv.js
+++ routes/online-cv.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/onlinecv", (req, res) => {
-    return res.render("online-cv.ejs")
+const middleware = require("../middleware/index");
+
+
+router.get("/onlinecv", middleware.isLoggedIn, (req, res) => {
+    User.findById(req.user._id, (err, user) => {
+        if (err) {
+            console.log(err);
+            return res.redirect("back");
+        }
+        return res.render("online-cv.ejs")
+    })
 })
 
-router.post('/onlinecv', (req, res) => {
-    
-})
+// router.post('/onlinecv', (req, res) => {
+//     req.user.body.cv = {
+//         awards: req.body.cv.awards,
+//         skills: req.body.cv.skills,
+
+//     }
+//     User.findByIdAndUpdate(req.user._id, req.user.body)
+// })
 
 module.exports = router;
 
