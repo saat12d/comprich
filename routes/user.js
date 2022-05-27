e20d59d0627c6231a82e792e232ef55f29c23db9
Saatvik <saatvik.s12@gmail.com>
2022-05-27 23:47:47 +0400

    fix

--- routes/user.js
+++ routes/user.js
@@ -203,7 +203,7 @@ router.post("/forgot", (req, res, next) => {
             },
             function (token, user, done) {
                 let smtpTransport = nodemailer.createTransport({
-                    host: "comprich.org",
+                    host: "Gmail",
                     auth: {
                         user: process.env.GMAILACC,
                         pass: process.env.GMAILPW,
